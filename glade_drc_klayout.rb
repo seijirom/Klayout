20893a5589bb3b022366cea31d1c40fbcee47e2d
Anagix Support <support@anagix.com>
2019-05-10 10:05:35 +0900

    Klayout for MakeLSI:

---
+++ glade_drc_klayout.rb
@@ -0,0 +1,16 @@
+while l=gets
+  if l=~/^geomSpace\((\S+), *(\S+), *(\S+), *(\".*\")\)/
+    layer1 = $1.downcase
+    layer2 = $2.downcase
+    separation = $3+'.um'
+    msg = $4
+    puts '# ' + l
+    puts "r_#{layer1}_#{layer2}_s = #{layer1}.separation(#{layer2}, #{separation}); r_#{layer1}_#{layer2}_s.output(#{msg})"
+  elsif l=~/^geomSpace\((\S+), *(\S+), *(\".*\")\)/
+    layer = $1.downcase    
+    space = $2+ '.um'
+    msg = $3
+    puts '# ' + l
+    puts "r_#{layer}_s = #{layer}.space(#{space}); r_#{layer}_s.output(#{msg})"
+  end
+end
