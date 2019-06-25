9a8403ef9aaf61f7e77e2c273c1dd9e5a90aaeba
Seijiro Moriyama <seijiro.moriyama@anagix.com>
2019-06-25 11:05:43 +0900

    glade_drc_klayout.rb under rev.

--- glade_drc_klayout.rb
+++ glade_drc_klayout.rb
@@ -4,13 +4,17 @@ while l=gets
     layer2 = $2.downcase
     separation = $3+'.um'
     msg = $4
-    puts '# ' + l
+    puts '# ' + l.chomp + " =>\n"
     puts "r_#{layer1}_#{layer2}_s = #{layer1}.separation(#{layer2}, #{separation}); r_#{layer1}_#{layer2}_s.output(#{msg})"
   elsif l=~/^geomSpace\((\S+), *(\S+), *(\".*\")\)/
     layer = $1.downcase    
     space = $2+ '.um'
     msg = $3
-    puts '# ' + l
+    puts '# ' + l.chomp + " =>\n"
     puts "r_#{layer}_s = #{layer}.space(#{space}); r_#{layer}_s.output(#{msg})"
+  elsif l=~/^#/
+    puts '# ' + l
+  else
+    puts l
   end
 end
